972cb057eb3775514649282c575e4eecc6c1890b
David Hoenisch <dh1689@pm.me>
2021-12-13 11:37:19 -0700

    added in ssh key functionality

--- Projects/File_nabber/file-grab.py
+++ Projects/File_nabber/file-grab.py
@@ -17,7 +17,7 @@ def welcome():
     print("===========================================")
     time.sleep(.2)
 
-def main(FILE, DESTINATION, OUT, ssh, host_key, no_key, connect, scp_client):
+def main(FILE, DESTINATION, OUT, USER, KEY, PASS, ssh, host_key, no_key, connect, scp_client):
     with open('grab-files.csv') as read_object:
         csv_reader = csv.DictReader(read_object)
         path = []
@@ -30,16 +30,25 @@ parser = optparse.OptionParser()
 parser.add_option("-f", "--file", dest="FILE", type="str", help="Specify CSV file to read from")
 parser.add_option("-d", "--destination", dest="DESTINATION", type="str", help="Specify destiniation host")
 parser.add_option("-o", "--output", dest="OUT", type="str", help="Specify where to save files that are downloaded")
+parser.add_option("-k", "--key", dest="KEY", type="str", help="Define ssh key to be use for authenitcation")
+parser.add_option("-u", "--user", dest="USER", type="str", help="Define user to authenticate as")
+parser.add_option("-p", "--password", dest="PASS", type="str", help="Define password to use in place of SSH key")
 (options, arguments) = parser.parse_args()
 
 ssh = SSHClient()
 host_key = ssh.load_system_host_keys()
 no_key = ssh.set_missing_host_key_policy(AutoAddPolicy())
-connect = ssh.connect(options.DESTINATION, username='root', password='YOUR-PASSWORD-HERE')
-scp_client = SCPClient(ssh.get_transport())
+
+if options.KEY == "":
+    connect = ssh.connect(options.DESTINATION, username=options.USER, passphrase=options.PASS)
+    scp_client = SCPClient(ssh.get_transport())
+else:
+    connect = ssh.connect(options.DESTINATION, username=options.USER, key_filename=options.KEY)
+    scp_client = SCPClient(ssh.get_transport())
+
 
 welcome()
-main(options.FILE, options.DESTINATION, options.OUT, ssh, host_key, no_key, connect, scp_client)
+main(options.FILE, options.DESTINATION, options.OUT, options.KEY, options.USER, options.PASS, ssh, host_key, no_key, connect, scp_client)
 scp_client.close()
 
 
